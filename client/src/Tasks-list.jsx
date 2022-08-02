8fc4cda770ca05033f7471d670736d27b6b38d14
VKozhura <kozhura_v@mail.ru>
2022-08-02 23:32:52 +0300

    change config

--- client/src/Tasks-list.jsx
+++ client/src/Tasks-list.jsx
@@ -1,28 +1,26 @@
 import React from "react";
-import axios from 'axios';
-import { Buffer } from 'node:buffer';
+import axios from "axios";
 
 import { dateConvert } from "./functions";
 
 const TasksList = () => {
-    const [tasks, setTasks] = React.useState();
-    const [startDate, setStartDate] = React.useState(null)
-    const [finishDate, setFinishDate] = React.useState(null)
+	const [tasks, setTasks] = React.useState();
+	const [startDate, setStartDate] = React.useState(null);
+	const [finishDate, setFinishDate] = React.useState(null);
 
-    const onStartChange = (e) => {
-        const value = e.target.value;
-        setStartDate(value);
-    }
-    const onFinishChange = (e) => {
-        const value = e.target.value;
-        setFinishDate(value);
-    }
+	const onStartChange = (e) => {
+		const value = e.target.value;
+		setStartDate(value);
+	};
+	const onFinishChange = (e) => {
+		const value = e.target.value;
+		setFinishDate(value);
+	};
 
-    console.log(Date.parse(new Date (startDate)));
-    console.log(Date.parse(new Date (finishDate)));
-    
+	console.log(Date.parse(new Date(startDate)));
+	console.log(Date.parse(new Date(finishDate)));
 
-    // React.useEffect(() => {
+	// React.useEffect(() => {
 	// 	const getTasks = () => {
 	// 		const config = {
 	// 			method: "get",
@@ -33,95 +31,96 @@ const TasksList = () => {
 	// 			},
 	// 		};
 	// 		axios(config).then((response) => {
-    //             console.log(response.data)
+	//             console.log(response.data)
 	// 			setTasks(response.data.tasks);
-                
+
 	// 		});
 	// 	};
 	// 	getTasks();
-  
+
 	// }, []);
- 
-    console.log(tasks)
 
-    const onTasks = async () => {
-        const userName = 'vkozhura';
-        const password = 'jrsoVsyYXm';
-        const token = `${userName}:${password}`;
-        const encodedToken = Buffer.from(token).toString('base64');
-        const config = {
-            method: "get",
-            // url: `http://localhost:5000/api?date1=${(Date.parse(new Date (startDate)))/1000}&date2=${(Date.parse(new Date (finishDate)))/1000}`,
-            url: 'https://redmine.bivgroup.com/issues.json',
-            headers: {
-                'Authorization': 'Basic '+ encodedToken,
-                "Content-Type": "application/json",
-                Accept: "application/json",
-                // 'X-Redmine-API-Key': 'c4bb0c5363355760be678f1ed6e30de09b3495d2',
-                'Access-Control-Allow-Origin': "http://localhost:3000",
-                // 'Access-Control-Allow-Credentials': true
-              
-            },
-        };
-        const res = await axios(config)
-        console.log(res.data)
-        // axios(config).then((response) => {
-        //     console.log(response.data)
-        //     setTasks(response.data.tasks);
-            
-        // });
-    }
+	console.log(tasks);
 
-    return (
-        <div>
-        <input type="date" onChange={onStartChange}/>
-        <input type="date" onChange={onFinishChange}/>
-        <button onClick={onTasks}>Сформировать</button>
+	const onTasks = () => {
+		const userName = "vkozhura";
+		const password = "jrsoVsyYXm";
+		const token = `${userName}:${password}`;
+		const url = "https://redmine.bivgroup.com/issues.json";
+		// const encodedToken = Buffer.from(token).toString('base64');
+		const instance = axios.create({
+			withCredentials: true,
+		});
+		const config = {
+			method: "get",
+			// url: `http://localhost:5000/api?date1=${(Date.parse(new Date (startDate)))/1000}&date2=${(Date.parse(new Date (finishDate)))/1000}`,
+			url: "https://redmine.bivgroup.com/issues.json",
+			headers: {
+				// 'Authorization': 'Basic '+ encodedToken,
+				"Content-Type": "application/json",
+				Accept: "application/json",
+				// "X-Redmine-API-Key": "c4bb0c5363355760be678f1ed6e30de09b3495d2",
+				"Access-Control-Allow-Origin": "http://localhost:3000",
+				auth: {
+					username: "vkozhura",
+					password: "jrsoVsyYXm",
+				},
+				withCredentials: true,
+			},
+		};
 
-        {tasks ? (<table>
-        <thead>
-            <tr>
-                <th>№ п/п</th>
-                <th>Задача</th>
-                <th>Трекер</th>
-                <th>Тема</th>
-                <th>Статус</th>
-                <th>Приоритет</th>
-                <th>Трудозатраты (в ч.)</th>
-                <th>Создано</th>
-                <th>Обновлено</th>
-                <th>Закрыто</th>
-                <th>Создано_Решено</th>
-                <th>Интервал</th>
-            </tr>
-        </thead>
-        <tbody>
-            {tasks.map((task, index) => (
-                <tr key={task.tasknumber}>
-                    <th>{index + 1}</th>
-                    <th>{task.tasknumber}</th>
-                    <th>{task.tracker}</th>
-                    <th>{task.theme}</th>
-                    <th>{task.status}</th>
-                    <th>{task.priority}</th>
-                    <th>{task.labour}</th>
-                    <th>{dateConvert(task.createdate)}</th>
-                    <th>{dateConvert(task.closeddate)}</th>
-                    <th>{dateConvert(task.updatedate)}</th>
-                    <th>{(task.createclosed/60).toFixed(2)}</th>
-                    <th>{(task.interv/60).toFixed(2)}</th>
-                </tr>
-            ))}
-        </tbody>
-           
-       </table>) : null }
-            
-       
-       </div>
-    )
-}
+		axios(config).then((response) => {
+			console.log(response.data);
+			setTasks(response.data.tasks);
+		});
+	};
 
+	return (
+		<div>
+			<input type="date" onChange={onStartChange} />
+			<input type="date" onChange={onFinishChange} />
+			<button onClick={onTasks}>Сформировать</button>
 
-
+			{tasks ? (
+				<table>
+					<thead>
+						<tr>
+							<th>№ п/п</th>
+							<th>Задача</th>
+							<th>Трекер</th>
+							<th>Тема</th>
+							<th>Статус</th>
+							<th>Приоритет</th>
+							<th>Трудозатраты (в ч.)</th>
+							<th>Создано</th>
+							<th>Обновлено</th>
+							<th>Закрыто</th>
+							<th>Создано_Решено</th>
+							<th>Интервал</th>
+						</tr>
+					</thead>
+					<tbody>
+						{tasks.map((task, index) => (
+							<tr key={task.tasknumber}>
+								<th>{index + 1}</th>
+								<th>{task.tasknumber}</th>
+								<th>{task.tracker}</th>
+								<th>{task.theme}</th>
+								<th>{task.status}</th>
+								<th>{task.priority}</th>
+								<th>{task.labour}</th>
+								<th>{dateConvert(task.createdate)}</th>
+								<th>{dateConvert(task.closeddate)}</th>
+								<th>{dateConvert(task.updatedate)}</th>
+								<th>{(task.createclosed / 60).toFixed(2)}</th>
+								<th>{(task.interv / 60).toFixed(2)}</th>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			) : null}
+		</div>
+	);
+};
 
 export default TasksList;
